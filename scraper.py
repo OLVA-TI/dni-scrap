a07cb07656bfbbfbf1e494c79edda6afcbd85528
Jhon Baudin <jhonbaup0895@gmail.com>
2024-06-27 18:06:49 -0500

    fix

--- scraper.py
+++ scraper.py
@@ -30,7 +30,7 @@ def init_browser():
     global browser_instance
     if browser_instance is None:
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
