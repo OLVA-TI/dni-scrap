885794fa7b9e53af1edf3398db40e37ac2cdfaa6
Jhon Baudin <jhonbaup0895@gmail.com>
2024-06-28 18:45:37 -0500

    fix

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ def init_browser():
     global browser_instance
     if browser_instance is None:
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
@@ -38,7 +38,7 @@ def init_browser():
         options.add_argument('--disable-notifications')
         options.add_argument('--disable-background-timer-throttling')
         options.add_argument('--disable-backgrounding-occluded-windows')
-        options.add_argument('--start-maximized')  # Iniciar Chrome maximizado
+        # options.add_argument('--start-maximized')  # Iniciar Chrome maximizado
         options.add_argument('--page-load-strategy=eager')
 
         prefs = {
